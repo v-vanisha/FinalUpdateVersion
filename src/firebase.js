315abeb3f9f76e5fb812b3e06182b3b1881582c9
vanishavaish <github.session2021@gmail.com>
2022-05-05 00:00:38 +0530

    final firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,35 +5,15 @@ import {getStorage} from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {getFirestore} from 'firebase/firestore';
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDVA8W7vED6cRKGSUxVaHJqPar0yDfUCTw",
-//     authDomain: "nexumforu.firebaseapp.com",
-//     projectId: "nexumforu",
-//     storageBucket: "nexumforu.appspot.com",
-//     messagingSenderId: "100165520823",
-//     appId: "1:100165520823:web:934c83bdd1ee5cbdc4ac32"
-// };
 
-
-//temporary
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB2ej0Q0zUeFv4WqbMP8HH3dUm39Tf9Nrg",
-//     authDomain: "tempnexum.firebaseapp.com",
-//     projectId: "tempnexum",
-//     storageBucket: "tempnexum.appspot.com",
-//     messagingSenderId: "371420502627",
-//     appId: "1:371420502627:web:ebb1c91ea2fdaa2de1b656"
-//   };
-
-  //vanisha_nexumBlog for firebase quota
+  //final firebase 
   const firebaseConfig = {
-    apiKey: "AIzaSyAwwt1_3PtoJbS8N9GwOSVWF2nR5b06YR4",
-    authDomain: "nexum-blog-d37e2.firebaseapp.com",
-    projectId: "nexum-blog-d37e2",
-    storageBucket: "nexum-blog-d37e2.appspot.com",
-    messagingSenderId: "25114350200",
-    appId: "1:25114350200:web:402c737dde5f7504f9138d"
+    apiKey: "AIzaSyCYhGUn4Zeaf-zACZ4B2orvNdJWq9HFdTQ",
+    authDomain: "nexumbest.firebaseapp.com",
+    projectId: "nexumbest",
+    storageBucket: "nexumbest.appspot.com",
+    messagingSenderId: "333151438394",
+    appId: "1:333151438394:web:83bbd93114bb78ee1e5703"
   };
 
 // Initialize Firebase
